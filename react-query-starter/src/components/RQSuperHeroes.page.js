a2ff4c5ebd0be30b77c02541d5583eb5d036cee9
Deepak_Chaurasiya <deepak.chaurasiya@indusnet.co.in>
2023-01-02 23:04:59 +0530

    React-Query-Learning

---
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -0,0 +1,28 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes');
+};
+export const RQSuperHeroesPage = () => {
+  const { isLoading, data, isError, error } = useQuery(
+    'super-heroes',
+    fetchSuperHeroes,
+    {cacheTime:5000},
+    {staleTime:30000}
+  );
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
+};
